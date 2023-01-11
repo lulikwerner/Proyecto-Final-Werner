a1f7257011f0587b6ac7eac9674187ebf0999bdb
Lucila Werner <lulikwerner@gmail.com>
2023-01-11 14:54:23 -0500

    search working

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ class Person {
 }
 
 /*Creo ya pacientes en el array para poder hacer las comparaciones necesarias en las opciones que tiene el doctor sin tener que cargar muchos pacientes*/ 
-let peopl = [{ userName:"Sophie", lastName: "Werner", gender: "mujer", age: 70, weight:63, height:170, TDCI:1234 , CARBS:23,FAT:45,PROT :600 },
-               {userName: "Thomas", lastName: "Perez", gender: "varon", age: 18,weight: 89, height: 190, TDCI: 1900 , CARBS: 123, FAT: 78, PROT:950 },
-               {userName: "Austin", lastName: "Fernandez", gender: "mujer", age: 39, weight: 76 , height: 187 , TDCI: 1350 , CARBS: 785, FAT: 56, PROT: 340 },
-               {userName: "Felicitas", lastName: "Zabuleta", gender: "varon", age: 50, weight: 1.66, height: 166, TDCI: 1200 , CARBS: 985, FAT: 87, PROT: 506 },
+let peopl = [{ userName:"SOPHIE", lastName: "WERNER", gender: "MUJER", age: 70, weight:63, height:170, TDCI:1234 , CARBS:23,FAT:45,PROT :600 },
+               {userName: "THOMAS", lastName: "PEREZ", gender: "VARON", age: 18,weight: 89, height: 190, TDCI: 1900 , CARBS: 123, FAT: 78, PROT:950 },
+               {userName: "AUSTIN", lastName: "FERNANDEZ", gender: "VARON", age: 39, weight: 76 , height: 187 , TDCI: 1350 , CARBS: 785, FAT: 56, PROT: 340 },
+               {userName: "FELICITAS", lastName: "ZABULETA", gender: "MUJER", age: 50, weight: 1.66, height: 166, TDCI: 1200 , CARBS: 985, FAT: 87, PROT: 506 },
             ];
 
 //Llama al modal del Login
@@ -479,21 +479,17 @@ deleteRow.addEventListener("click",function(){
 /*Busca en el search box*/
 let newPeopl='';
 document.getElementById('myInput').addEventListener("keyup",function(){
-    showPatients(peopl)
     let search = this.value.toUpperCase();
     newArray = peopl.filter(function(val){
         if(val.userName.includes(search)|| val.lastName.includes(search)||val.gender.includes(search)){
            newPeopl=[{userName: val.userName, lastName: val.lastName, gender: val.gender, age: val.age, weight:val.weight, 
                 height:val.height, TDCI:val.TDCI , CARBS:val.CARBS,FAT:val.FAT,PROT :val.PROT}]
-               
-                 showPatients(newPeopl); 
-             
-                
+                return newPeopl;
             }
         
-            
+           
         })
-     
+        showPatients(newArray); 
 })
 
 
